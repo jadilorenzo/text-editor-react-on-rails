c441af48894e43dfb2d4c32c464679613426fb76
Jacob Di Lorenzo <jadilorenzo@icloud.com>
2022-11-27 08:56:35 -0500

    Fix selection bugs

--- app/javascript/packs/ReactTextEditor/components/TextEditor.tsx
+++ app/javascript/packs/ReactTextEditor/components/TextEditor.tsx
@@ -22,32 +22,37 @@ export default function TextEditor() {
   return (
     <div className='text-editor surface'>
       {document.document.length === 0 ? <Cursor/> : null}
-      {document.document.map((element, index) => {
-        const insideSelection = document.selection ? (
-          index >= document.selection.start && index <= document.selection.end - 1
-        ) : (selectionStartIndex !== undefined) ? (
-          (index >= selectionStartIndex && index <= hoverSelectionIndex) || (index <= selectionStartIndex && index >= hoverSelectionIndex)
-        ) : false
-        return (
-          <span 
-            key={JSON.stringify({element, index})}
-            onMouseDown={() => setSelectionStartIndex(index)} 
-            onMouseUp={() => {
-              if (selectionStartIndex !== index) {
-                setDocument((prevDocument) => prevDocument.setSelection({start: (selectionStartIndex || 0) + ((selectionStartIndex || 0) > index ? +1 : 0 ), end: index + ((selectionStartIndex || 0) > index ? 0 : +1)}))
-                setSelectionStartIndex(undefined)
-              }
-            }}
-            onMouseEnter={() => setHoverSelectionIndex(index)}
-            style={{background: insideSelection ? '#90CAF9' : undefined}}
-          >
-            {document.position === index ? <Cursor/> : null}
-            {(element.type === 'EOL') ? (
-              <br/>
-            ) : <Element element={element} />}
-          </span>
-        )
-      })}
+      <span onMouseLeave={() => {
+        setHoverSelectionIndex(0)
+        setSelectionStartIndex(undefined)
+      }}>
+        {document.document.map((element, index) => {
+          const insideSelection = document.selection ? (
+            index >= document.selection.start && index <= document.selection.end - 1
+          ) : (selectionStartIndex !== undefined) ? (
+            (index >= selectionStartIndex && index <= hoverSelectionIndex) || (index <= selectionStartIndex && index >= hoverSelectionIndex)
+          ) : false
+          return (
+            <span 
+              key={JSON.stringify({element, index})}
+              onMouseDown={() => setSelectionStartIndex(index)} 
+              onMouseUp={() => {
+                if (Math.abs(selectionStartIndex || 0 - index) > 1) {
+                  setDocument((prevDocument) => prevDocument.setSelection({start: (selectionStartIndex || 0) + ((selectionStartIndex || 0) > index ? +1 : 0 ), end: index + ((selectionStartIndex || 0) > index ? 0 : +1)}))
+                  setSelectionStartIndex(undefined)
+                }
+              }}
+              onMouseEnter={() => setHoverSelectionIndex(index)}
+              style={{background: insideSelection ? '#90CAF9' : undefined}}
+            >
+              {document.position === index ? <Cursor/> : null}
+              {(element.type === 'EOL') ? (
+                <br/>
+              ) : <Element element={element} />}
+            </span>
+          )
+        })}
+      </span>
     </div>
   )
 }
