221b04587bdcecf31574fa2856077a2f4dc8f48c
Jacob Di Lorenzo <jadilorenzo@icloud.com>
2022-12-02 21:16:01 -0500

    Major code refactor

---
+++ app/javascript/packs/ReactTextEditor/context/TextEditorContext.tsx
@@ -0,0 +1,79 @@
+import React, { createContext, useState } from "react"
+import useHandleDocument from "../hooks/useHandleDocument"
+import Document from "../../models/BaseDocument"
+
+export const TextEditorContext = createContext<any>({})
+
+const TextEditorProvider = (props: { children: any }) => {
+  // to force update upon class change
+  const [, updateState] = React.useState({});
+  const forceUpdate = React.useCallback(() => updateState({}), []);
+  // will require you to say "window.document." vs. "document."
+  const [document, setDocumentState] = useState(new Document())
+  const setDocument = (props) => {
+    setDocumentState(props)
+    forceUpdate()
+  }
+
+  const [selectionStartIndex, setSelectionStartIndex] = useState<number | undefined>(undefined)
+  const [hoverSelectionIndex, setHoverSelectionIndex] = useState(0)
+  const [activeStyles, setActiveStyles] = useState<string[]>([])
+  
+  const toggleActiveStyles = (style: string) => {
+    console.log(style)
+    setActiveStyles((prevStyles) => {
+      if (prevStyles.includes(style)) {
+        return prevStyles.filter(prevStyle => prevStyle !== style)
+      } else {
+        return [...prevStyles, style]
+      }
+    })
+  }
+
+  
+  const toggleBoldStyle = () => {
+    setDocument((prevDocument) => prevDocument.styleSelection({ style: 'bold' }))
+    toggleActiveStyles('bold')
+  }
+  
+  const toggleItalicStyle = () => {
+    setDocument((prevDocument) => prevDocument.styleSelection({ style: 'italics' }))
+    toggleActiveStyles('italics')
+  }
+  
+  const toggleUnderlinedStyle = () => {
+    setDocument((prevDocument) => prevDocument.styleSelection({ style: 'underlined' }))
+    toggleActiveStyles('underlined')
+  }
+  
+  const toggleStrikethroughStyle = () => {
+    setDocument((prevDocument) => prevDocument.styleSelection({ style: 'strikethrough' }))
+    toggleActiveStyles('strikethrough')
+  } 
+  
+  useHandleDocument({ 
+    document, setDocument, 
+    activeStyles, toggleActiveStyles,
+    toggleBoldStyle,
+    toggleItalicStyle,
+    toggleUnderlinedStyle,
+    toggleStrikethroughStyle
+  })
+
+  return (
+    <TextEditorContext.Provider value={{
+      document, setDocument,
+      selectionStartIndex, setSelectionStartIndex,
+      hoverSelectionIndex, setHoverSelectionIndex,
+      toggleBoldStyle,
+      toggleItalicStyle,
+      toggleUnderlinedStyle,
+      toggleStrikethroughStyle,
+      activeStyles
+    }}>
+      {props.children}
+    </TextEditorContext.Provider>
+  )
+}
+
+export default TextEditorProvider
